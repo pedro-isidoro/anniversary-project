dd45bec3dc6fbecdd7cdc4f772134a6400ff4639
Pedro <pedro.isidoro26@yahoo.com.br>
2023-05-27 12:58:24 -0300

    Atualizando.

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+// h3's of the li
+const optionTitle = document.getElementById(optionTitle);
+console.log(optionTitle)
+
+// li's
+const options = document.getElementById(option);
+console.log(options)
+
+// optionTitle.addEventListener('click', () => {
+
+// });
