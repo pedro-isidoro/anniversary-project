3927327fa3e525588b896ecdca4cee83b06bcccd
Pedro <pedro.isidoro26@yahoo.com.br>
2023-05-29 09:17:30 -0300

    Atualizando.

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,18 @@
-// h3's of the li
-const optionTitle = document.getElementById(optionTitle);
-console.log(optionTitle)
-
 // li's
-const options = document.getElementById(option);
-console.log(options)
+const optionsToTables = document.querySelectorAll(".option");
+console.log(optionsToTables);
 
-// optionTitle.addEventListener('click', () => {
+// h3's of the li
+// const optionTitle = document.querySelectorAll(".optionTitle");
+// console.log(optionTitle);
 
-// });
+optionsToTables.forEach(function(option){
+    // console.log(option);
+    option.addEventListener("click", function() {
+        const currentActiveOption = document.querySelector(".active");
+        if(currentActiveOption){
+            currentActiveOption.classList.remove("active")
+        }
+        option.classList.add("active");
+    });
+});
